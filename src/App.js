64f48510ec56e953368991c6446df8ec6378f2c1
Yashita-Namdeo <yashita.namdeo2000@gmail.com>
2021-07-22 22:27:56 +0530

    built components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Model from "./components/Model";
+import Stories from "./components/Stories";
+import Create from "./components/Create";
+import Posts from "./components/Posts";
+import Sidebar from "./components/Sidebar";
+import Context from "./Global/Context";
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Context>
+      <Navbar />
+      <div className="container">
+        <Stories />
+        <Create />
+        <Posts />
+        <Sidebar />
+      </div>
+      <Model />
+    </Context>
   );
 }
 
